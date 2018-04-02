4f8d4e349a72abbe01ab4dab465a1468353d3b37
Jeff Brubaker <jtbrubak1@gmail.com>
2018-04-02 16:25:52 -0400

    Finish image uploader

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', img=False)
+
+@app.route('/submit', methods=["POST"])
+def submit():
+    f = request.files['image']
+    f.save('assets/image.jpg')
+    return render_template('index.html', img=True)
+
+@app.route('/image')
+def send_file():
+    return send_from_directory('assets', 'image.jpg')
