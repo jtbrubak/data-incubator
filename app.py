7377599adaf2b1d4b6aabecee75538592f8cafbf
Jeff Brubaker <jtbrubak1@gmail.com>
2018-04-02 17:05:47 -0400

    Change to http response

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask, render_template, request, url_for, send_from_directory
+from flask import Flask, render_template, request, make_response
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', img=False)
+    return render_template('index.html')
 
-@app.route('/submit', methods=["POST"])
+@app.route('/image', methods=["POST"])
 def submit():
-    f = request.files['image']
-    f.save('assets/image.jpg')
-    return render_template('index.html', img=True)
-
-@app.route('/image')
-def send_file():
-    return send_from_directory('assets', 'image.jpg')
+    content_type = request.files['image'].content_type
+    response = make_response(request.files['image'].read())
+    response.headers.set('Content-Type', content_type)
+    return response
