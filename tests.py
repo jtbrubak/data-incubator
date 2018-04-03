b71f5963529dd761c80a7eb6be032c78ff604ce7
Jeff Brubaker <jtbrubak1@gmail.com>
2018-04-03 10:28:30 -0400

    Change python version

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class TestCase(unittest.TestCase):
         self.app = data_incubator.app.test_client()
 
     def test_image_post(self):
-        with open('test_image.png') as f:
+        with open('test_image.png', encoding="utf-16") as f:
             raw_img = f.read()
         rv = self.app.post('/image', content_type='multipart/form-data', data={'image': (BytesIO(raw_img), 'test_image.png')})
         assert rv.data == raw_img
